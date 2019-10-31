bf28309e99fea24810e971be6f15e73258a3601c
minor19 <57132871+minor19@users.noreply.github.com>
2019-10-31 13:54:43 +0100

    Update Play.java

--- Play.java
+++ Play.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 
 public class Play{
 	// First I determine the attributes needed.
-	public String name1;
-	public String autoplayer;
-	Scanner Keyboard = new Scanner(System.in);
-	System.out.Println("Enter your name player1"); //Hvis man selv skal teste sit navn ind.
-	name1 = keyboard.nextLine();
-	private static Player player1=new Player(name1);
-	private static Player player2=new Player(autoplayer);
-	private static Board game=new Board(player1,player2);
-	
+	public static String name1;
+	public static String autoplayer;
+
 	// Main method.
 	public static void main(String[]args){
-		game.trumpcard(); // Shows Trump-card
-		System.out.println("The Trump Card is"+trumpcard)
+		System.out.println("A Card Game by Christian, Mikkel and Rasmus");
 		
-		player1.hand(); //returns a copy of the cards in the player's hand
-		System.out.println(player1+"your hand is" + hand)
-		// Choose who goes first.
-			public static void chooseFirstPlayer(){
-			game.next(); 		// picks the player to go first
-    }
-		System.out.println("Testing...");
-		System.out.println();
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter your name below:"); //Hvis man selv skal teste sit navn ind.
+		name1 = keyboard.nextLine();
 		
-		// MÅSKE INDFØR ET INTERFACE MED INTRO TIL SPIL OG FORKLARING AF SPILLET.
+		Player player1=new Player(name1);
+		Player player2=new Player(autoplayer);
+		Board game=new Board(player1,player2);
 		
-		while(!game.gameOver){
-			// LOOP DUEL
-		}
+		System.out.println(" ");
+		System.out.println("The rules are the following:");
+		System.out.println("1. At the start of the game each player starts with 3 cards.");
+		System.out.println("2. The winner of the previous round begins the current round (with the exception of the first round where it is decided between the players).");
+		System.out.println("3. Then a Trump Suit will be decided.");
+		System.out.println("4. If the second player plays a card of higher value in the same suit as the first player, then he/she wins both cards.");
+		System.out.println("5. If the second player plays a card of the Trump Suit and the first player does not, then the second player wins both cards.");
+		System.out.println("6. Ohterwise, the first player wins the round.");
 		
-		chooseFirstPlayer(); // Choose who goes first
-        declareWinner(); // Declare a winner
-		wonLast(); // Determines who won last
-	}
-
+		System.out.println(" ");		
+		System.out.println("To win the game:");
+		System.out.println("1. When there are no more cards in the deck, the players continue until they run out of cards in their hands.");
+		System.out.println("2. The scores are determined by the card value of; Ace, Seven, King, Jack and Queen. whose values are: 11, 10, 4, 3 and 2 respectively.");
+		
+		System.out.println(" ");
+		System.out.println("The Trump Card of this round is "+game.trumpCard());
+		
+		System.out.println(" ");
+		System.out.println(player1.hand());
+	}	
+}	
+	
+	
+-----------------------------------------------------------------------------------------------------------------------------------
     // Choose who goes first.
     public static void chooseFirstPlayer(){
         game.next(); 		// picks the player to go first
